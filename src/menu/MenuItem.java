0df0b682a1fae5567a12e562c6f59471d8037690
Adam Duhamel <Adam.Duhamel1996@gmail.com>
2017-08-17 16:44:55 -0700

    added Menu Item fields

--- src/menu/MenuItem.java
+++ src/menu/MenuItem.java
@@ -1,4 +1,13 @@
 package menu;
 
+import java.util.Date;
+
 public class MenuItem {
+    private String title;
+    private String description;
+    private double price;
+    private String category;
+    private boolean isNew;
+    private Date creationDate;
+
 }

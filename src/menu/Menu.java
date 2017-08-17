a53934478d6325ee7a152fce9e4fe700ea439046
Adam Duhamel <Adam.Duhamel1996@gmail.com>
2017-08-17 15:23:46 -0700

    added Menu fields

--- src/menu/Menu.java
+++ src/menu/Menu.java
@@ -1,4 +1,12 @@
 package menu;
 
+import java.util.Date;
+import java.util.HashMap;
+
 public class Menu {
+    private HashMap<String, MenuItem> appitizerItems = new HashMap<>();
+    private HashMap<String, MenuItem> mainCourseItems = new HashMap<>();
+    private HashMap<String, MenuItem> DessertItems = new HashMap<>();
+    private Date date = new Date();
+
 }

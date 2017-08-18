6ef2ad1ee332e0332ca8236135b1181fe95050a8
Adam Duhamel <Adam.Duhamel1996@gmail.com>
2017-08-18 16:34:14 -0700

    added a long date

--- src/menu/MDY.java
+++ src/menu/MDY.java
@@ -3,10 +3,14 @@ package menu;
 import java.util.Date;
 
 public class MDY extends Date{
+    public final long old = 1209600000;
     public static void main(String[] args){
         System.out.println("date:");
-
         System.out.println(MDY.getMMDDYY());
+
+        System.out.println("\nlong:");
+        System.out.println(MDY.getLong());
+
     }
     public static String getMMDDYY(){
         MDY date = new MDY();
@@ -14,5 +18,10 @@ public class MDY extends Date{
         int month = date.getMonth() + 1;
         int day = date.getDate();
         return (String.valueOf(month) + "/" + String.valueOf(day) + "/" + String.valueOf(year));
+    }
+    public static long getLong(){
+        MDY date = new MDY();
+        long DT = date.getTime();
+        return DT;
     }
 }
